52ca3ee581933406cbc6959bc2ee37fdabd58800
Michael Daigle <mcdaigle1@gmail.com>
2026-05-02 21:19:52 -0700

    periodic update

--- finally_animatronics_api/src/main.py
+++ finally_animatronics_api/src/main.py
@@ -1,18 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 
 app = FastAPI(title="hello-api")
 
 @app.get("/")
-
 def hello() -> dict[str, str]:
     return {"message": "Hello, world"}
 
 @app.get("/healthz")
-
 def healthz() -> dict[str, str]:
     return {"status": "ok"}
 
 @app.get("/readyz")
-
 def readyz() -> dict[str, str]:
     return {"status": "ready"}
+
+@app.get("/favicon.ico", include_in_schema=False)
+def favicon():
+    return Response(status_code=204)
