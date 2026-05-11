80c911cc2c98d148ccbc619ec064ed1b936c528e
Michael Daigle <mcdaigle1@gmail.com>
2026-05-10 20:24:51 -0700

    add code to write to and read from pvs

--- finally_animatronics_api/src/main.py
+++ finally_animatronics_api/src/main.py
@@ -1,10 +1,33 @@
-from fastapi import FastAPI, Response
+from fastapi import FastAPI, Response, Request
+from datetime import datetime, UTC
+
+import json
 
 app = FastAPI(title="hello-api")
 
+LOG_FILE = "/data/requests.jsonl"
+
 @app.get("/")
-def hello() -> dict[str, str]:
-    return {"message": "Hello, world"}
+def hello(request: Request) -> dict[str, str]:
+    log_entry = {
+        "timestamp": datetime.now(UTC).isoformat(),
+        "client_host": request.client.host if request.client else None,
+        "method": request.method,
+        "headers": dict(request.headers),
+        "url": str(request.url)
+    }
+
+    with open(LOG_FILE, "a") as f:
+        f.write(json.dumps(log_entry) + "\n")
+
+    with open(LOG_FILE, "r") as f:
+        log_contents = f.read()
+
+
+    return {
+        "message": "Hello, world",
+        "requests": log_contents
+    }
 
 @app.get("/healthz")
 def healthz() -> dict[str, str]:
